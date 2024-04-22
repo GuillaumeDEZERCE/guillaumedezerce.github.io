c87be6d5bc1950d49441abd1712913aea9891308
GuillaumeDEZERCE <guillaume.dezerce@gmail.com>
2024-04-22 16:37:46 +0200

    corection responsive mineur

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 const burger = document.querySelector('.burger');
 const main_nav = document.querySelector('.main-nav');
+const nav_link = document.querySelectorAll('.nav-link');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     main_nav.classList.toggle('active');
 })
+
+console.log(nav_link);
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// nav_link.addEventListener('click', () => {
+//     burger.classList.toggle('active');
+//     main_nav.classList.toggle('active');
+// })
 
