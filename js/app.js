d7da55a9109f4fb3c6837838555ad9212a5d5a01
GuillaumeDEZERCE <guillaume.dezerce@gmail.com>
2024-04-22 15:35:27 +0200

    mise en forme de la nouvelle barre de navigation, de la palette de couleur et correction mineurs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const burger = document.querySelector('.burger');
+const main_nav = document.querySelector('.main-nav');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
+    main_nav.classList.toggle('active');
 })
+
