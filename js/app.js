0675311bbbc02275ec39c7b6f4009571289f9b83
GuillaumeDEZERCE <guillaume.dezerce@gmail.com>
2024-04-24 11:55:58 +0200

    hidden du scroll quand la pop up des mentions légales est ouverte et possibilité de fermer la pop up en cliquant en dehors de la fenêtre

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ console.log(nav_link);
 // legal notice
 
 // récupération de l'encart des mentions légales
+const legal_notice_background = document.querySelector('.legal-notice-background');
 const legal_notice = document.querySelector('.legal-notice');
 
 // récupération du lien
@@ -37,7 +38,11 @@ function handleLegalNoticeClick(event) {
     event.preventDefault();
 
     // ajout de la classe legal-notice-on sur les mentions légales
+    legal_notice_background.classList.add('legal-notice-on');
     legal_notice.classList.add('legal-notice-on');
+
+    // suppression du scroll sur l'axe Y
+    document.documentElement.style.overflowY = 'hidden';   
 }
 
 // récupération du bouton de fermeture de la boîte mentions légales
@@ -45,10 +50,15 @@ const closeElement = document.querySelector('.legal-notice-close');
 
 // on pose un écouteur sur le bouton
 closeElement.addEventListener('click', handleClickLegalNoticeCross);
+legal_notice_background.addEventListener('click', handleClickLegalNoticeCross);
 
 // Fonction qui ferme l'encart mentions légales
 function handleClickLegalNoticeCross() {
 
     // suppression de la classe legal-notice-on sur legal-notice
+    legal_notice_background.classList.remove('legal-notice-on');
     legal_notice.classList.remove('legal-notice-on');
+    
+    // ajout du scroll sur l'axe Y
+    document.documentElement.style.overflowY = 'scroll'; 
 }
