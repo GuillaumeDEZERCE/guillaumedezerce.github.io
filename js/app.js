bb9fa3b949be0b8552e08d942fbcb2cc8ba7bae5
GuillaumeDEZERCE <guillaume.dezerce@gmail.com>
2024-04-24 19:35:02 +0200

    Fermeture menu burger quand click sur un lien

--- js/app.js
+++ js/app.js
@@ -1,64 +1,77 @@
-// menu burger
+// hamburger menu
 
 const burger = document.querySelector('.burger');
 const main_nav = document.querySelector('.main-nav');
 const nav_link = document.querySelectorAll('.nav-link');
+// const main = document.getElementsByTagName('main');
+// const footer = document.getElementsByTagName('footer');
+
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     main_nav.classList.toggle('active');
 })
 
-console.log(nav_link);
+// main.addEventListener('click', () => {
+//      burger.classList.toggle('active');
+//      main_nav.classList.toggle('active');
+// })
 
-// // for (let index = 0; index < array.length; index++) {
-// //     const element = array[index];
-    
-// // }
-// // nav_link.addEventListener('click', () => {
-// //     burger.classList.toggle('active');
-// //     main_nav.classList.toggle('active');
-// // })
+// footer.addEventListener('click', () => {
+//      burger.classList.toggle('active');
+//      main_nav.classList.toggle('active');
+// })
+
+
+
+for (const link of nav_link) {
+    link.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        main_nav.classList.toggle('active');
+    })
+}
 
 // legal notice
 
-// récupération de l'encart des mentions légales
-const legal_notice_background = document.querySelector('.legal-notice-background');
+// recovery of the legal notice insert and its background
 const legal_notice = document.querySelector('.legal-notice');
+const legal_notice_background = document.querySelector('.legal-notice-background');
 
-// récupération du lien
+// recovery of the legal notice link
 const legal_notice_button = document.querySelector('#legal-notice-btn');
 
-// ajout de l'écouteur d'événéments
+// recovery of the close button for the legal notice pop-up
+const closeElement = document.querySelector('.legal-notice-close');
+
+
+// event listener added
 legal_notice_button.addEventListener('click', handleLegalNoticeClick);
 
-// Définition de la fonction qui est exécutée lors du clic sur le lien
+// function that is executed when the link is clicked
 function handleLegalNoticeClick(event) {
     
     event.preventDefault();
 
-    // ajout de la classe legal-notice-on sur les mentions légales
+    // legal-notice-on class added to legal notices and its background
     legal_notice_background.classList.add('legal-notice-on');
     legal_notice.classList.add('legal-notice-on');
 
-    // suppression du scroll sur l'axe Y
+    // scroll deleted to Y axis
     document.documentElement.style.overflowY = 'hidden';   
 }
 
-// récupération du bouton de fermeture de la boîte mentions légales
-const closeElement = document.querySelector('.legal-notice-close');
 
-// on pose un écouteur sur le bouton
+// event listener added on the button
 closeElement.addEventListener('click', handleClickLegalNoticeCross);
 legal_notice_background.addEventListener('click', handleClickLegalNoticeCross);
 
-// Fonction qui ferme l'encart mentions légales
+// Function that closes the legal notices pop-up
 function handleClickLegalNoticeCross() {
 
-    // suppression de la classe legal-notice-on sur legal-notice
-    legal_notice_background.classList.remove('legal-notice-on');
+    // legal-notice-on class deleted to legal-notice and its background
     legal_notice.classList.remove('legal-notice-on');
+    legal_notice_background.classList.remove('legal-notice-on');
     
-    // ajout du scroll sur l'axe Y
+    // scroll added to Y axis
     document.documentElement.style.overflowY = 'scroll'; 
 }
