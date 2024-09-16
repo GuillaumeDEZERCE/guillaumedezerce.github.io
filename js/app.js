ff0c353104c30d919f1f634a5a4502bdf3a6f289
GuillaumeDEZERCE <guillaume.dezerce@gmail.com>
2024-09-16 19:26:20 +0200

    Corrections mineur dans le html et changement de couleur du hover sur le navbar

--- js/app.js
+++ js/app.js
@@ -33,40 +33,39 @@ var aText = new Array(
     "Guillaume Dezerce,",
     "Développeur web",
     "Full-Stack / Back"
-    );
-    var iSpeed = 100; // time delay of print out
-    var iIndex = 0; // start printing array at this posision
-    var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
-     
-    var iTextPos = 0; // initialise text position
-    var sContents = ''; // initialise contents variable
-    var iRow; // initialise current row
-     
-    function typewriter()
-    {
-     sContents =  ' ';
-     iRow = Math.max(0, iIndex-iScrollAt);
-     var destination = document.querySelector('h1');
-     
-     while ( iRow < iIndex ) {
-      sContents += aText[iRow++] + '<br />';
-     }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "<span>_</span>";
-     if ( iTextPos++ == iArrLength ) {
-      iTextPos = 0;
-      iIndex++;
-      if ( iIndex != aText.length ) {
-       iArrLength = aText[iIndex].length;
-       setTimeout("typewriter()", 500);
-      }
-     } else {
-      setTimeout("typewriter()", iSpeed);
-     }
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+    
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+    
+function typewriter()
+{
+    sContents =  ' ';
+    iRow = Math.max(0, iIndex-iScrollAt);
+    var destination = document.querySelector('h1');
+    
+    while ( iRow < iIndex ) {
+    sContents += aText[iRow++] + '<br />';
     }
-    
-    
-    typewriter();
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "<span>_</span>";
+    if ( iTextPos++ == iArrLength ) {
+    iTextPos = 0;
+    iIndex++;
+    if ( iIndex != aText.length ) {
+    iArrLength = aText[iIndex].length;
+    setTimeout("typewriter()", 500);
+    }
+    } else {
+    setTimeout("typewriter()", iSpeed);
+    }
+}
+
+typewriter();
 
 // legal notice
 
